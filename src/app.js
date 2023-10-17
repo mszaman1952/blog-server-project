e74b88b7525de62b534413169b58d7f8b657a037
mszaman1952 <125687107+mszaman1952@users.noreply.github.com>
2023-10-17 07:38:07 +0600

    [IN PROGRESS] server settings

--- src/app.js
+++ src/app.js
@@ -2,7 +2,40 @@ import express from express;
 import cors from cors;
 import httpStatus from "http-status";
 
+const app = express();
+
 // use the middleware 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extends : true}));
 
+// app.use('/api/v1', router);
+// app.get(env)
 
+app.get("/", (req,res) =>{
+    res.send("Server is Running....");
+});
 
+// app.use((err, req, res, next) => {
+//     res.status(err.status || httpStatus.NOT_FOUND).send({
+//         error : {
+//             status : err.status || httpStatus.NOT_FOUND,
+//             message : err.message
+//         }
+//     })
+// })
+
+app.use((req, res) => {
+    res.status(httpStatus.NOT_FOUND).json({
+        success : false,
+        message : "Route Not Found!!",
+        errorMessages : [
+            {
+                path : "",
+                message : "Route Not Found!!"
+            }
+        ]
+    })
+})
+
+export default app;
